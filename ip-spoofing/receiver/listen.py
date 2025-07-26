3a9bc4731517a173a136e935b5df00c154a8403b
IshrakAdit <ishrak.adit07@gmail.com>
2025-07-26 12:47:10 +0600

    spoofing: finite

--- ip-spoofing/receiver/listen.py
+++ ip-spoofing/receiver/listen.py
@@ -1,12 +1,17 @@
 from scapy.all import sniff, IP, ICMP, send, Raw
 
+packet_count = 0  
+
 def auto_reply(pkt):
-    print(f"Got packet: {pkt.summary()}")
+    global packet_count
     if pkt.haslayer(ICMP) and pkt[ICMP].type == 8:
+        packet_count += 1
+        print(f"[{packet_count}] Got packet: {pkt.summary()}")
+        
         ip = IP(src=pkt[IP].dst, dst=pkt[IP].src)
         icmp = ICMP(type=0, id=pkt[ICMP].id, seq=pkt[ICMP].seq)
         payload = ("Reply to: " + pkt[Raw].load.decode(errors='ignore')) if pkt.haslayer(Raw) else ""
-        reply = ip/icmp/payload
+        reply = ip / icmp / payload
         send(reply, verbose=0)
         print(f"[+] Replied to spoofed ping from {pkt[IP].src}")
 
